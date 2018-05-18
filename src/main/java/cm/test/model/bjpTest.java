eea54a35740eb74e37304902161057717ebf59e8
HSarode-Compumatrice <hsarode@edrnet.com>
2018-05-18 18:00:06 +0530

    Create bjpTest.java

---
+++ src/main/java/cm/test/model/bjpTest.java
@@ -0,0 +1,45 @@
+package cm.test.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.util.Date;
+
+
+
+@Entity
+@Table(name = "bjptest")
+
+
+public class bjpTest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String remark;
+    private Date date;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+    
+    
+	
+}
